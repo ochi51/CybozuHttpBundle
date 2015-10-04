33d2d6b919b2cca942a98cfc5e0cb82c9864bd61
yuichi ochiai <ochiai07@gmail.com>
2015-10-04 12:48:26 +0900

    fixes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace CybozuHttpBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 
 /**
  * @author ochi51 <ochiai07@gmail.com>
@@ -17,26 +18,33 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('cybozu_http');
-        $rootNode
-            ->children()
-                ->scalarNode('domain')->defaultValue('cybozu.com')->end()
-                ->scalarNode('subdomain')->defaultNull()->end()
-                ->booleanNode('useApiToken')->defaultFalse()->end()
-                ->scalarNode('login')->defaultNull()->end()
-                ->scalarNode('password')->defaultNull()->end()
-                ->scalarNode('token')->defaultNull()->end()
-                ->booleanNode('useBasic')->defaultFalse()->end()
-                ->scalarNode('basicLogin')->defaultNull()->end()
-                ->scalarNode('basicPassword')->defaultNull()->end()
-                ->booleanNode('useClientCert')->defaultFalse()->end()
-                ->scalarNode('certFile')->defaultNull()->end()
-                ->scalarNode('certPassword')->defaultNull()->end()
-                ->scalarNode('cert_dir')->defaultNull()->end()
-                ->booleanNode('debug')->defaultFalse()->end()
-                ->scalarNode('logfile')->defaultNull()->end()
-                ->booleanNode('test')->defaultFalse()->end()
-            ->end()
-        ;
+        $this->setChildrenNodes($rootNode);
+
         return $treeBuilder;
+    }
+
+    /**
+     * @param NodeDefinition $rootNode
+     */
+    private function setChildrenNodes(NodeDefinition $rootNode)
+    {
+        $rootNode->children()
+            ->scalarNode('domain')->defaultValue('cybozu.com')->end()
+            ->scalarNode('subdomain')->defaultNull()->end()
+            ->booleanNode('useApiToken')->defaultFalse()->end()
+            ->scalarNode('login')->defaultNull()->end()
+            ->scalarNode('password')->defaultNull()->end()
+            ->scalarNode('token')->defaultNull()->end()
+            ->booleanNode('useBasic')->defaultFalse()->end()
+            ->scalarNode('basicLogin')->defaultNull()->end()
+            ->scalarNode('basicPassword')->defaultNull()->end()
+            ->booleanNode('useClientCert')->defaultFalse()->end()
+            ->scalarNode('certFile')->defaultNull()->end()
+            ->scalarNode('certPassword')->defaultNull()->end()
+            ->scalarNode('cert_dir')->defaultNull()->end()
+            ->booleanNode('debug')->defaultFalse()->end()
+            ->scalarNode('logfile')->defaultNull()->end()
+            ->booleanNode('test')->defaultFalse()->end()
+        ->end();
     }
 }
